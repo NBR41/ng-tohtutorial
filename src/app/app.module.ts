a95db6566c2b0c6d6b2503e78f1d42565a5efaea
fbernard <fabien.bernard@twenga.com>
2017-08-02 14:17:41 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-
-import { HeroDetailComponent } from './hero-detail.component';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent }     from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+      BrowserModule,
+      FormsModule,
+      RouterModule.forRoot([
+          { path: 'heroes', component: HeroesComponent },
+          { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+          { path: 'dashboard', component: DashboardComponent },
+          { path: 'detail/:id', component: HeroDetailComponent },
+
+      ])
+  ],
+  declarations: [
+      AppComponent,
+      HeroesComponent,
+      HeroDetailComponent,
+      DashboardComponent
+  ],
+  providers: [
+      HeroService
+  ],
+  bootstrap: [
+      AppComponent
+  ]
 })
 export class AppModule { }
